ecf2e66951e4b4028e9bef842a43b841c1f9143b
chenxiuqi <xiuqichen8@gmail.com>
2017-06-28 11:07:04 -0700

    Added post.swift file to send image and caption details to Parse

--- instagram/ViewController.swift
+++ instagram/ViewController.swift
@@ -7,19 +7,28 @@
 //
 
 import UIKit
+import Parse
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBAction func onLogout(_ sender: Any) {
+        PFUser.logOutInBackground { (error: Error?) in
+        }
+        NotificationCenter.default.post(name: NSNotification.Name("logoutNotification"), object:nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
